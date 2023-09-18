a9f6e7176e4874841952a95e3d702e6baf939fe4
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-09-18 14:17:44 +0400

    Need to check

--- AD/List all Admin Users.ps1
+++ AD/List all Admin Users.ps1
@@ -1,2 +1,9 @@
-﻿$group = gwmi win32_group -filter 'Name = "Administrators"'
+# Need to check again
+ 
+ $group = gwmi win32_group -filter 'Name = "Administrators"'
 $group.GetRelated('Win32_UserAccount') | Out-GridView
+
+#List Administrators 
+Get-ADGroupMember -Identity "Administrators" -Recursive | Select-Object "*" | Export-CSV "C:\temp\Administrators.csv" -NoTypeInformation -Encoding UTF8 
+
+ 
