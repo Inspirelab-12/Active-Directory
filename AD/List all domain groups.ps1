c9e46d090c7019a3ffccd23fed85860429d8e6cf
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-09-19 14:42:24 +0400

    Update

--- AD/List all domain groups.ps1
+++ AD/List all domain groups.ps1
@@ -1,3 +1,7 @@
 # List all domain groups
 
 Get-ADGroup -Filter * | select SamAccountName, objectClass, GroupCategory, GroupScope | ft -AutoSize | Out-String -Width 4096
+
+# List all Domain Groups Export CSV
+ 
+Get-ADGroup -Filter * | select SamAccountName, objectClass, GroupCategory, GroupScope | ft -AutoSize | Export-CSV "C:\temp\Domain Groups.csv" -NoTypeInformation -Encoding UTF8 
