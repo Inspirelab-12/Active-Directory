580b946005287d65be830ac934a62ba403d3029e
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-09-19 10:26:42 +0400

    Update

--- AD/List specific properties from domain computers.ps1
+++ AD/List specific properties from domain computers.ps1
@@ -1,4 +1,5 @@
 # List specific properties from domain computers.
 
-Get-ADComputer -Filter * -Properties * | select Name, Samaccountname, Enabled, DistinguishedName | Format-Table
+Get-ADComputer -Filter * -Properties * | select Name, Samaccountname, Enabled, DistinguishedName | Export-CSV "C:\temp\AllADComputers.csv" -NoTypeInformation -Encoding UTF8  
+
 
