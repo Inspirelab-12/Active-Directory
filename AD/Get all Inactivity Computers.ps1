0055e967d244495fadcec5650f27dd3402dea522
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-09-19 10:11:49 +0400

    Update

--- AD/Get all Inactivity Computers.ps1
+++ AD/Get all Inactivity Computers.ps1
@@ -1,6 +1,6 @@
 ﻿#Specify inactivity range value below
 
-$DaysInactive = 10
+$DaysInactive = 90
 
 #time variable converts $DaysInactive to LastLogonTimeStamp property format for the -Filter switch to work
 
@@ -8,4 +8,4 @@ $time = (Get-Date).Adddays(-($DaysInactive))
 
 #Identify and collect inactive computer accounts:
 
-Get-ADComputer -Filter {LastLogonTimeStamp -lt $time} -ResultPageSize 2000 -resultSetSize $null -Properties Name, OperatingSystem, SamAccountName, DistinguishedName, LastLogonDate| Export-CSV “C:\Temp\InactivityComputers.CSV” –NoTypeInformation
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $time} -ResultPageSize 2000 -resultSetSize $null -Properties Name, OperatingSystem, SamAccountName, DistinguishedName, LastLogonDate | Export-CSV “C:\Temp\InactivityComputers90Days.csv” –NoTypeInformation
