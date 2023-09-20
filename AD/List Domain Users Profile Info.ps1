98de294ba6477eefade7db62f5fca00335172f7f
Alex.SG <alex.mailme12@gmail.com>
2023-09-20 14:16:25 +0400

    Update List Domain Users Profile Info.ps1

--- AD/List Domain Users Profile Info.ps1
+++ AD/List Domain Users Profile Info.ps1
@@ -1,6 +1,7 @@
-﻿# List Domain Users Profile Info
+﻿
 
 
+# List Domain Users Profile Info
 Get-ADUser -filter * -properties PasswordExpired, PasswordLastSet, PasswordNeverExpires, EmailAddress, Title, Manager | ft Name, PasswordExpired, PasswordLastSet, PasswordNeverExpires, EmailAddress, Title, Manager 
 
 
