22fcff17a67a9ebe3d1d73b0db5bc4d1797231e0
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-09-20 11:42:54 +0400

    Add files via upload

---
+++ AD/FSMO Rolles Transfer.ps1
@@ -0,0 +1,5 @@
+﻿# Move-ADDirectoryServerOperationMasterRole -Identity "ADC1" PDCEmulator
+# Move-ADDirectoryServerOperationMasterRole -Identity "ADC1" RIDMaster
+# Move-ADDirectoryServerOperationMasterRole -Identity "ADC1" Infrastructuremaster
+# Move-ADDirectoryServerOperationMasterRole -Identity "ADC1" DomainNamingmaster
+# Move-ADDirectoryServerOperationMasterRole -Identity "ADC1" SchemaMaster
