880a08dfd3df34c799ca727b08ed7572e65bba5a
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-10-03 08:35:15 +0400

    Update List AD Users.ps1

--- AD/List AD Users.ps1
+++ AD/List AD Users.ps1
@@ -6,5 +6,5 @@ Get-ADUser -filter * -properties * | Export-CSV "C:\temp\ADUsers.csv" -NoTypeInf
 
 #List AD Users with Object Selection
 #---------------------------------------------------------------------------------------------------------------------------
-Get-ADUser -filter * -properties * | Select-Object Displayname, ObjectClass, title, department, samaccountname, EmailAddress, LastLogonDate, PasswordExpired, PasswordLastSet, PasswordNeverExpires | Export-CSV "C:\temp\ADUsers.csv" -NoTypeInformation -Encoding UTF8  
+Get-ADUser -filter * -properties * | Select-Object Displayname, Samaccountname, ObjectClass, title, department, EmailAddress, mobile, LastLogonDate, PasswordExpired, PasswordLastSet, PasswordNeverExpires | Export-CSV "C:\temp\ADUsers.csv" -NoTypeInformation -Encoding UTF8  
 #---------------------------------------------------------------------------------------------------------------------------
