d15aff2afddb327fe34851fce2f54e70dc852256
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-10-03 11:02:30 +0400

    Update List AD Users Properties.ps1

--- AD/List AD Users Properties.ps1
+++ AD/List AD Users Properties.ps1
@@ -8,27 +8,27 @@
 # --------------------------------------------------------------------- #
 
 
-# List AD Selected User Properties Export CSV
+# 1. List AD Selected User Properties Export CSV
 # --------------------------------------------------------------------------------------------------------------------------- 
  Get-ADUser -Identity test1 -Properties * | Export-CSV "C:\temp\ADUsers.csv" -NoTypeInformation -Encoding UTF8
 # --------------------------------------------------------------------------------------------------------------------------- 
 
-# List AD User Properties Export CSV
+# 2. List AD User Properties Export CSV
 # --------------------------------------------------------------------------------------------------------------------------- 
 Get-ADUser -filter * -properties * | Export-CSV "C:\temp\ADUsers.csv" -NoTypeInformation -Encoding UTF8 
 # ---------------------------------------------------------------------------------------------------------------------------
 
-# List AD Selected User with Selected Objects Export CSV
+# 3. List AD Selected User with Selected Objects Export CSV
 # ---------------------------------------------------------------------------------------------------------------------------
 Get-ADUser -Identity test1 -properties * | Select-Object Displayname, Samaccountname, ObjectClass, title, department, EmailAddress, mobile, whenCreated, LastLogonDate, PasswordExpired, PasswordLastSet, PasswordNeverExpires | Export-CSV "C:\temp\AD-test1-Users.csv" -NoTypeInformation -Encoding UTF8  
 # ---------------------------------------------------------------------------------------------------------------------------
 
-# List AD Users with Selected Objects Export CSV
+# 4. List AD Users with Selected Objects Export CSV
 # ---------------------------------------------------------------------------------------------------------------------------
 Get-ADUser -filter * -properties * | Select-Object Displayname, Samaccountname, ObjectClass, title, department, EmailAddress, mobile, whenCreated, LastLogonDate, PasswordExpired, PasswordLastSet, PasswordNeverExpires | Export-CSV "C:\temp\ADUsers.csv" -NoTypeInformation -Encoding UTF8  
 # ---------------------------------------------------------------------------------------------------------------------------
 
-# List information for both Enabled and Disabled Users
+# 5. List information for both Enabled and Disabled Users
 # ---------------------------------------------------------------------------------------------------------------------------
 # Get information for both enabled and disabled users
 $allUsers = Get-ADUser -Filter * -Properties Name, Displayname, Samaccountname, ObjectClass, title, department, EmailAddress, mobile, whenCreated, LastLogonDate, PasswordExpired, PasswordLastSet, PasswordNeverExpires, Enabled  
