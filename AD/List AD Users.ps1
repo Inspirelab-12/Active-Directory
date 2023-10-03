03a620ac9284a10bcc8a9ace93916748b8fc415c
Alex.SG <32876354+alex0918@users.noreply.github.com>
2023-10-03 07:59:05 +0400

    Update List AD Users.ps1

--- AD/List AD Users.ps1
+++ AD/List AD Users.ps1
@@ -1,3 +1,10 @@
 #List AD Users 
- 
+--------------------------------------------------------------------------------------------------------------------------- 
 Get-ADUser -filter * -properties * | Export-CSV "C:\temp\ADUsers.csv" -NoTypeInformation -Encoding UTF8 
+---------------------------------------------------------------------------------------------------------------------------
+
+
+#List AD Users with Object Selection
+---------------------------------------------------------------------------------------------------------------------------
+Get-ADUser -filter * -properties * | Select-Object Displayname, Desscription, title, department, samaccountname, lastlogin | Export-CSV "C:\temp\ADUsers.csv" -NoTypeInformation -Encoding UTF8  
+---------------------------------------------------------------------------------------------------------------------------
