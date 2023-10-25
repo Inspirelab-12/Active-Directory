8d9b965888d8e3c6ebed520d9e6575d54f616869
Alex.SG <alex.mailme12@gmail.com>
2023-10-25 09:33:20 +0400

    V03

--- Reports/To retrieve information about the installed Application on multiple Client PCs using PowerShell.ps1
+++ Reports/To retrieve information about the installed Application on multiple Client PCs using PowerShell.ps1
@@ -1,10 +1,51 @@
 
 # To retrieve information about the installed Application on multiple Client PCs using PowerShell #
 
-foreach ($computer in (Get-Content C:\test\computers422.txt)) {
+# Index 
+# 1. For All Products Multiple Client
+# 2. For All Products Current Machine 
+# 3. For Particular Product Multiple Client (Ex: O365ProPlusRetail)
+# 3. For Particular Product Current Machine (Ex: O365ProPlusRetail)
+
+
+# ------------------------------------------------------------------------------------------------------- #
+
+# 1. For All Products 
+
+foreach ($computer in (Get-Content C:\Script_tests\computers.txt)) {
   write-verbose "Working on $computer..." -Verbose
   Invoke-Command -ComputerName "$Computer" -ScriptBlock {
     Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
-    Select-Object DisplayName, DisplayVersion, Publisher
-  } | export-csv C:\test\results.csv -Append -NoTypeInformation
+    Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+  } | export-csv C:\Script_tests\results.csv -Append -NoTypeInformation
 }
+
+# ------------------------------------------------------------------------------------------------------- #
+
+# 2. For All Products Current Machine 
+
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  
+Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+Format-Table –AutoSize
+
+# ------------------------------------------------------------------------------------------------------- #
+
+# 3. For Particular Product (Ex: O365ProPlusRetail)
+
+foreach ($computer in (Get-Content C:\Script_tests\computers.txt)) {
+  write-verbose "Working on $computer..." -Verbose
+  Invoke-Command -ComputerName "$Computer" -ScriptBlock {
+    Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\O365ProPlusRetail* |
+    Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+  } | export-csv C:\Script_tests\results.csv -Append -NoTypeInformation
+}
+
+# ------------------------------------------------------------------------------------------------------- #
+
+#4. For Particular Product Current Machine (Ex: O365ProPlusRetail)
+
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\O365ProPlusRetail* |  
+Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+Format-Table –AutoSize
+
+# ------------------------------------------------------------------------------------------------------- #
