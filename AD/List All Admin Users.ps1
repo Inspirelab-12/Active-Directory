287dd03422e6f5f12ac12078301992a5b9b90f22
Alex.SG <alex.mailme12@gmail.com>
2023-11-23 16:15:57 +0400

    23112023

--- AD/List All Admin Users.ps1
+++ AD/List All Admin Users.ps1
@@ -1,6 +1,45 @@
 
-#List Administrators 
+#List All Administrators 
 Get-ADGroupMember -Identity "Administrators" -Recursive | Select-Object "*" | Export-CSV "C:\temp\AllAdministrators.csv" -NoTypeInformation -Encoding UTF8 
+
+#--------------------------
+
+# Domain Admin Users 
+
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Specify the domain controller
+$domainController = "ADAVIATION.LOCAL"
+
+# Specify the distinguished name of the Domain Admins group
+$domainAdminsGroup = "CN=Domain Admins,CN=Users,DC=ADAVIATION,DC=LOCAL"
+
+# Get Domain Admins group members
+$adminUsers = Get-ADGroupMember -Identity $domainAdminsGroup
+
+# Create Report Date & Year 
+$LogDate = get-date -f ddMMyyyyhhmm
+
+# Specify the path for the CSV file
+$csvFilePath = "C:\reports\AdminUsers_$logDate.csv"
+
+# Create an array to store the results
+$results = @()
+
+# Populate the array with user information
+foreach ($adminUser in $adminUsers) {
+    $userInfo = [PSCustomObject]@{
+        "Name"          = $adminUser.Name
+        "SamAccountName"= $adminUser.SamAccountName
+    }
+    $results += $userInfo
+}
+
+# Export the results to a CSV file
+$results | Export-Csv -Path $csvFilePath -NoTypeInformation
+
+Write-Host "Admin users exported to: $csvFilePath"
 
  
 
--- AD/Remote uninstall SQL Server Management Studio software via powershell.ps1
+++ AD/Remote uninstall SQL Server Management Studio software via powershell.ps1
@@ -1,22 +1,16 @@
-# List Installed Softwares #
+# To uninstall the software remotely, you should adhere to the following steps.
 
-Get-WmiObject -Class Win32_Product | Select-Object -Property Name
-
-# ------------------------------------------------------------------------------------- #
-
-#Remote PowerShell# 
+#Remote Login powerShell
 Enable-PSRemoting
 Enter-PSSession -ComputerName dc01 -Credential $Cred
 
-# ------------------------------------------------------------------------------------- #
-
-# Remote Access Testing #
+# Remote Access Test 
 Get-Process | Get-Member
 
-# ------------------------------------------------------------------------------------- #
+# List Installed Softwares
+Get-WmiObject -Class Win32_Product | Select-Object -Property Name
 
-# Uninstall Softwares #
-
+# Uninstall Softwares 
 $SQLVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object { $_.DisplayName -match "Microsoft SQL Server Management Studio" } | Select-Object -Property DisplayName, UninstallString
 
 ForEach ($ver in $SQLVer) {
@@ -30,4 +24,4 @@ ForEach ($ver in $SQLVer) {
 
 }
 
-# ------------------------------------------------------------------------------------- #
+# <-------------------------------------------Alex.SG ------------------------>
