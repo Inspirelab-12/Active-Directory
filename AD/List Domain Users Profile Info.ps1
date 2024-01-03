8c97528fbf8e848f54d657df4b034d325cbce384
Alex.SG <32876354+alex0918@users.noreply.github.com>
2024-01-03 08:00:06 +0400

    Update List Domain Users Profile Info.ps1

--- AD/List Domain Users Profile Info.ps1
+++ AD/List Domain Users Profile Info.ps1
@@ -4,5 +4,5 @@
 # List Domain Users Profile Info
 Get-ADUser -filter * -properties PasswordExpired, PasswordLastSet, PasswordNeverExpires, EmailAddress, Title, Manager | ft Name, PasswordExpired, PasswordLastSet, PasswordNeverExpires, EmailAddress, Title, Manager 
 
-
+#<------------------------------------------------------>#
 
