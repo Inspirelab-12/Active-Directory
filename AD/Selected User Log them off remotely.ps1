4369995c0f4da01d8e9da0ac06fce424a15f4ead
Alex.SG <32876354+alex0918@users.noreply.github.com>
2025-04-11 08:01:23 +0400

    Add files via upload

---
+++ AD/Selected User Log them off remotely.ps1
@@ -0,0 +1,13 @@
+﻿$targets = @("ADADCHV08", "ADARADIUS", "ADANWBKP")
+foreach ($server in $targets) {
+    try {
+        $sessions = quser /server:$server | Where-Object { $_ -match "ent\.admin" }
+        foreach ($session in $sessions) {
+            $sessionId = ($session -split "\s+")[2]
+            logoff $sessionId /server:$server
+            Write-Host "Logged off ent.admin from $server session ID $sessionId"
+        }
+    } catch {
+        Write-Warning "Could not log off from $server - $_"
+    }
+}
